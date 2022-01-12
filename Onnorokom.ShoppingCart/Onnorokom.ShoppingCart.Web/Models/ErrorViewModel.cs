9cd14f6a94766045b8bec455d451662c81136f61
Saikat30101997 <saikat30101997@gmail.com>
2022-01-13 00:07:03 +0600

    add initial project

---
+++ Onnorokom.ShoppingCart/Onnorokom.ShoppingCart.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Onnorokom.ShoppingCart.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
